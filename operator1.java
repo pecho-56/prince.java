5964d8ec65f132d06d64d0dae5ae924c55ee842b
pecho-56 <prince861076@gmail.com>
2025-03-13 11:47:41 +0530

    rohini college

---
+++ operator1.java
@@ -0,0 +1,12 @@
+public class operator1{
+    public static void main(String[]args){     
+        int a=20;
+        int b=30;
+        String c="apple";
+        String d="hello";
+        System.out.println(a==b);
+        System.out.println(a>=b);
+        System.out.println(c==d);
+
+    }
+}
