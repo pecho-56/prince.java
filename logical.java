f7d3d83636ba10c90db3171cdaa0ae3be0db4367
pecho-56 <prince861076@gmail.com>
2025-03-13 12:10:16 +0530

    rohini college

---
+++ logical.java
@@ -0,0 +1,9 @@
+public class logical{
+    public static void main(String[]args){     
+        int a=2005;
+        int b=3000;
+        int c=2005;
+        System.out.println((a>b) && (a==b));
+        System.out.println((a>b) || (b>c));
+    }
+}
